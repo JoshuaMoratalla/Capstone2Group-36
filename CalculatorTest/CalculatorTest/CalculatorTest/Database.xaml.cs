3201b1a5b5bd216038dc2fc1dc801d0c788ecec5
JoshuaMoratalla <joshuamoratalla87@gmail.com>
2018-03-13 20:15:18 +1000

    Navigation and Calcuator Integration

---
+++ CalculatorTest/CalculatorTest/CalculatorTest/Database.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorTest
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Database : ContentPage
+	{
+		public Database ()
+		{
+			InitializeComponent ();
+        }
+        protected override bool OnBackButtonPressed() {
+            App.Current.MainPage = new MainPage();
+            return base.OnBackButtonPressed();
+        }
+
+	}
+}
